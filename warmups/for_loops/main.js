95d0ef0a081ace67eb08d79a3430d5555a7a5d02
Travis Hewitt <travishewitt@Traviss-MacBook-Pro.local>
2017-07-20 09:40:51 -0600

    clean up and polish

--- warmups/for_loops/main.js
+++ warmups/for_loops/main.js
@@ -1,7 +1,7 @@
 for (var i = 0; i < 101; i++) {
     if (i % 2 === 0) {
-        console.log("even", i);
+        console.log(i + " is even!");
     } else {
-        console.log("odd", i);
+        console.log(i + " is odd!");
     }
 }
