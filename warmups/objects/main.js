e2ec52d2e15d2437373067c6cc9d05aa449169dd
Travis Hewitt <travishewitt@Traviss-MacBook-Pro.local>
2017-07-25 11:18:52 -0600

    object warmups

---
+++ warmups/objects/main.js
@@ -0,0 +1,86 @@
+var tea = {
+    type: "oolong", 
+    flavor1: "earthy", 
+    flavor2: "nutty", 
+    caffeineLevel: "medium", 
+    completeTaste: function(){
+        return this.flavor1 + " and " + this.flavor2
+    }
+};
+
+var bottleOfColdBrew = {
+    type: "delicious", 
+    amount: "not enough", 
+    price: 4,
+    drink: function() {
+        return "Drink me!";
+    }
+}
+
+
+var laptop = {
+    color: "spaceGray", 
+    name: "macBookPro",
+    size: 15,
+    battery: function() {
+        for (var batteryLeft = 100; batteryLeft > 0; batteryLeft--) {
+           if (batteryLeft > 0) {
+             console.log("battery status: " + batteryLeft + "%");
+        } else {
+            return "computer is going to die";
+        }
+    }
+}
+}
+
+
+var door = {
+    isItOpen: true,
+    color: "white",
+    isItLocked: false,
+    closeIt: function() {
+        return "close the door"
+    }
+}
+
+var cinnamonTea = {
+    amountOfSachets: 30,
+    flavor: "cinnamon",
+    caffeineLevel: "medium", 
+    caffeinated: function(amountOfSachets) {
+        if (amountOfSachets <= 2) {
+            return "you will be nice and invigorated";
+        } else {
+            return "you will shake, and die of overcaffeination, maybe";
+        }
+        }
+}
+
+var microphone = {
+    name: "blue yeti",
+    color: "gray",
+    doIHaveOne: true,
+    number: 4,
+    identifier: function () {
+        return "hello, my name is " + this.name + ", but i'm not actually blue, i'm " + this.color + "."
+    }
+}
+
+var person = {
+    name: "sarah", 
+    friends: ["kimmy", "jimmy", "timmy"]
+        };
+
+//add a friend to the end of friends!
+
+person.friends.push("Bob");
+
+//remove Bob, because he's rude
+//.pop() removes the last value of the array, it doesn't require an argument
+
+//Assigning the .pop() method to a new variable, lastPerson becomes a string "Bob"
+var lastPerson = person.friends.pop();
+console.log(person.friends);
+//["kimmy", "jimmy", "timmy"]
+console.log(lastPerson);
+//"Bob"
