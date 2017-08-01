7534d8ff1cea2945eea015453f227763b2b0488a
Travis Hewitt <travis.j.hewitt@gmail.com>
2017-08-01 15:20:08 -0600

    refactored node calc

--- exercises/node_calc/app.js
+++ exercises/node_calc/app.js
@@ -1,18 +1,18 @@
 var readline = require("readline-sync")
 
-var num1 = readline.question('Please enter your first number: ');
-var num2 = readline.question('Please enter your second number: ');
+var num1 = parseInt(readline.question('Please enter your first number: '));
+var num2 = parseInt(readline.question('Please enter your second number: '));
 var operation = readline.question('Please enter the operation to perform: ');
 
 function operate(num1, num2, operation) {
     if (operation === "add") {
-        return "Result: " + (parseInt(num1) + parseInt(num2))
+        return "Result: " + num1 + num2
     } else if (operation === "sub") {
-        return "Result: " + (parseInt(num1) - parseInt(num2))
+        return "Result: " + num1 - num2
     } else if (operation === "mul") {
-        return "Result: " + (parseInt(num1) * parseInt(num2))
+        return "Result: " + num1 * num2
     } else if (operation === "div") {
-        return "Result: " + (parseInt(num1) / parseInt(num2))
+        return "Result: " + num1 / num2
     }
 }
 
