2bd6be968682f62cc38b8dd0ea89ebfc1355c3a3
Travis Hewitt <travis.j.hewitt@gmail.com>
2017-08-04 13:34:19 -0600

    some writing polishing on rpg practice

--- exercises/rpg_practice/app.js
+++ exercises/rpg_practice/app.js
@@ -1,6 +1,6 @@
 var readline = require("readline-sync");
 var keyFound = false;
-console.log("You wake up. Your head is pounding. You wipe the sweat off your brow, and notice blood. You wonder where the hell you are, and what the hell happened. You rock from side to side, hoping to alleviate the pain, but the rusty creaks of the bed make the headache worse. You fall off the bed and shield your eyes from the single blinding light overhead, and look at your options. There's a door ten feet in front of you, and a hole in the wall, two feet to the left of your face, and some dusty books, and moldy clothes under the bed. What do you do?")
+console.log("You wake up. Your head is pounding. You wipe the sweat off your brow, and notice blood. You wonder where the hell you are, and what the hell happened. You rock from side to side, hoping to alleviate the pain, but the rusty creaks of the bed make the headache worse. You fall off the bed and shield your eyes from the single blinding light overhead, and look at your options. There's a door ten feet in front of you, a hole in the wall, two feet to the left of your face, and some dusty books, and moldy clothes under the bed. What do you do?")
 var choices = ['Open the door', 'Look under the bed', 'Explore the hole in the wall'], index = readline.keyInSelect(choices, 'What do you do?');
     if (index === 0 && keyFound === false) {
         console.log("You get up, and stumble to the big, rusty, steel door. It doesn't budge. It's locked. But you see a key hole.")
@@ -10,7 +10,7 @@ var choices = ['Open the door', 'Look under the bed', 'Explore the hole in the w
                 keyFound = true;
                 var choices = ['Open the door!', 'Stay here'], index = readline.keyInSelect(choices, 'What do you do?');
                     if (index === 0 && keyFound) {
-                        console.log("You turn the key, and kick the door open. Natural sunlight floods the room, and you walk out. Hmm... what's next? Find out in chapter 2.")
+                        console.log("You turn the key, and kick the door open. Natural sunlight floods the room, and you walk out. Hmm... you wonder what the special is at McDonalds. You'll probably get what the special is today, but you already got your side of freedom.")
                     } else {
                         console.log("You are too scared of the unknown. What if my captor is waiting for me out there? What if I'm just going to get tortured? And you live out what little life you have left on the rusty bed, having your bones buzzed by the growl of the old light, and die of starvation.")
                     }
