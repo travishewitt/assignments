80a272b4d7d890256570fa73a0130d502704c15d
Travis Hewitt <travis.j.hewitt@gmail.com>
2017-08-04 13:46:20 -0600

    more polishing

--- exercises/rpg_practice/app.js
+++ exercises/rpg_practice/app.js
@@ -23,7 +23,7 @@ var choices = ['Open the door', 'Look under the bed', 'Explore the hole in the w
                 keyFound = true;
                 var choices = ['Open the door!', 'Stay here'], index = readline.keyInSelect(choices, 'What do you do?');
                     if (index === 0 && keyFound) {
-                        console.log("You turn the key, and kick the door open. Natural sunlight floods the room, and you walk out. Hmm... what's next? Find out in chapter 2.")
+                        console.log("You turn the key, and kick the door open. Natural sunlight floods the room, and you walk out. Hmm... you wonder what the special is at McDonalds. You'll probably get what the special is today, but you already got your side of freedom.")
                     } else {
                         console.log("You are too scared of the unknown. What if my captor is waiting for me out there? What if I'm just going to get tortured? And you live out what little life you have left on the rusty bed, having your bones buzzed by the growl of the old light, and die of starvation.")
                     }
